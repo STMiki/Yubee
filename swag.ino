041cfb33a55eb50691c62ae8da45942b6a8eb6de
STMiki <micou.veillet@gmail.com>
2017-03-24 10:03:29 +0100

    reglage bug mémoire
    donc j'ai modifier le programme de l'afficheur, et celui du site.

--- swag.ino
+++ swag.ino
@@ -3,25 +3,22 @@
 #define HT_DATA 2
 #define HT_WR   3
 #define HT_CS   4
-#define HT_CS2  5
 
 // use this line for single matrix
 Adafruit_HT1632LEDMatrix matrix = Adafruit_HT1632LEDMatrix(HT_DATA, HT_WR, HT_CS);
-// use this line for two matrices!
-//Adafruit_HT1632LEDMatrix matrix = Adafruit_HT1632LEDMatrix(HT_DATA, HT_WR, HT_CS, HT_CS2);
 
-void drawMatrix(byte tableau[16][24]) {
+void drawMatrix(long tableau[16]) {
+  long val;
 
   matrix.clearScreen();
-  uint8_t id_temp = matrix.height() - 1;
-
   for (uint8_t height=0; height<matrix.height(); height++) {
-    for (uint8_t width=0; width<matrix.width(); width++) {
-      if (tableau[id_temp][width] == 1) {
-        matrix.setPixel(width, height);
+    for (uint8_t width=0;width<matrix.width();width++) {
+      val=tableau[height] >> ((matrix.width() - 1) - width);
+      val=val & 0x01;
+      if (val == 1) {
+        matrix.setPixel(width, 15 - height);
       }
     }
-    id_temp--;
   }
   matrix.writeScreen();
 }
@@ -33,9 +30,30 @@ void setup() {
   delay(500);
   matrix.clearScreen(); 
 }
+
+long test[16] = {
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+  0b000001100000000000110000,
+  0b000010010000000001001000,
+  0b000010010000000001001000,
+  0b000001100000000000110000,
+  0b000000000000000000000000,
+  0b000001000000000000010000,
+  0b000000111111111111100000,
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+  0b000000000000000000000000,
+};
+
 //pour afficher un truc sur l'affichage : 
 //byte tableau_affichage[16][24] = "le code sortit du site 'Matrice.html'" (on utilise pas le ledControl. il faut décocher la case. et reverse.)
 
 void loop() {
-  //drawMatric(tableau_affichage);
+  drawMatrix(test);
+  delay(212121231231);
 }
